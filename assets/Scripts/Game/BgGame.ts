ada0e38b05acd3aa809037a29b4dae94b06d119a
hahuyvu975 <hahuyvu975@gmail.com>
2023-07-20 14:28:20 +0700

    Bg moving

--- assets/Scripts/Game/BgGame.ts
+++ assets/Scripts/Game/BgGame.ts
@@ -1,31 +1,40 @@
 import { GameModel } from './GameModel';
 import { _decorator, Component, Node, Vec3, Sprite, CCFloat, v3 } from 'cc';
-import { GameController } from './GameController';
-import { Ball } from './Ball';
+
 const { ccclass, property } = _decorator;
 
 @ccclass('BgGame')
 export class BgGame extends Component {
 
-//    @property({
-//        type: Node
-//    })
-//    private ball: Node
+    @property({ type: GameModel })
+    private gameModel: GameModel;
 
-   private posBall: Vec3 = new Vec3();
+    private bgStartPositionX: number;
+    private bgOffsetX: number = 0.0;
+
+    private spriteWidth: number;
 
     protected onLoad(): void {
-        // this.spriteWidth = this.node.getComponent(Sprite).spriteFrame.width;
-        // console.log(this.spriteWidth); //4643
+        this.spriteWidth = this.node.getComponent(Sprite).spriteFrame.width;
     }
 
     protected start(): void {
-        // this.bgStartPositionX = this.node.position.x;
+        this.bgStartPositionX = this.node.position.x;
     }
 
     protected update(dt: number): void {
-        // this.posBall.x = this.ball.position.x;
-        // this.node.setPosition(this.posBall.x, 0);
+        if (this.gameModel.IsMovingGame) {
+
+
+            this.bgOffsetX += this.gameModel.SpeedObject * dt;
+            let offset: number = 0.0;
+            if (this.bgOffsetX > this.spriteWidth) {
+                offset = this.bgOffsetX - this.spriteWidth;
+                this.bgOffsetX = 0.0;
+            }
+            this.node.position = v3(this.bgStartPositionX - this.bgOffsetX - offset, this.node.position.y, this.node.position.z);
+        }
     }
 }
+
 
--- assets/Scripts/Game/Camera.ts
+++ assets/Scripts/Game/Camera.ts
@@ -3,5 +3,24 @@ const { ccclass, property } = _decorator;
 
 @ccclass('Camera')
 export class Camera extends Component {
-   
+    @property({
+        type: Node
+    })
+    private ball: Node;
+
+
+    @property({
+        type: Node
+    })
+    private containerNode: Node = null; // Thêm thuộc tính containerNode vào script
+
+    protected update(deltaTime: number) {
+        // const ballPosition = this.ball.position;
+        // const cameraPosition = this.node.position;
+        // const containerInitialPosition = this.containerNode.position;
+        // const cameraNewPosition = new Vec3(ballPosition.x, cameraPosition.y, cameraPosition.z);
+
+        // // Cập nhật vị trí mới cho camera
+        // this.node.setPosition(cameraNewPosition);
+    }
 }
